c30fdc6e09bc5fe0c6fbacc07046e1f5beff2917
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-08-23 21:09:44 +0800

    update

--- dl-dev/build-image.sh
+++ dl-dev/build-image.sh
@@ -5,7 +5,11 @@ if [ "$python_ver" == "" ]; then
 fi
 echo "Using python version: ${python_ver}"
 
-docker build -t jianshao/dl-dev-base:$python_ver . --build-arg TAG=${python_ver}-slim
-docker push jianshao/dl-dev-base:$python_ver
+base_image=python
+docker pull ${base_image}:${python_ver}-slim
+
+image=jianshao/dl-dev
+docker build -t ${image}:$python_ver . --build-arg TAG=${python_ver}-slim
+docker push ${image}:$python_ver
 
 echo "Done"
