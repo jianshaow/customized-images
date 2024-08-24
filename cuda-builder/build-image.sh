8d5c0c2e2b76babe6a8a3236f56bfda612010b85
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-08-24 20:36:25 +0800

    update version

--- cuda-builder/build-image.sh
+++ cuda-builder/build-image.sh
@@ -6,10 +6,10 @@ fi
 echo "Using cuda version: ${cuda_ver}"
 
 base_image=nvidia/cuda
-docker pull ${base_image}:${cuda_ver}-devel-ubuntu24.04
+docker pull ${base_image}:${cuda_ver}-devel-ubuntu22.04
 
 image=jianshao/cuda-builder
-docker build -t ${image}:${cuda_ver} . --build-arg TAG=${cuda_ver}-devel-ubuntu24.04
+docker build -t ${image}:${cuda_ver} . --build-arg TAG=${cuda_ver}-devel-ubuntu22.04
 docker push ${image}:${cuda_ver}
 
 echo "Done"
