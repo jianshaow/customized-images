0b25bde86234f31a4bfe58a7ac839d844c5a14ae
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-08-26 21:19:43 +0800

    enhance

--- cpp-dev/README.md
+++ cpp-dev/README.md
@@ -1,7 +1,6 @@
 # cpp-dev
 
 ~~~ shell
-export image_ver=0.0.1
-docker build -t jianshao/cpp-dev:$image_ver .
-docker push jianshao/cpp-dev:$image_ver
+export ubuntu_ver=22.04
+./build-image.sh
 ~~~
---
+++ cpp-dev/build-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$ubuntu_ver" == "" ]; then
+    ubuntu_ver=22.04
+fi
+echo "Using ubuntu version: ${ubuntu_ver}"
+
+base_image=ubuntu
+docker pull ${base_image}:${ubuntu_ver}
+
+image=jianshao/cpp-dev
+
+docker build -t ${image}:latest . --build-arg TAG=${ubuntu_ver}
+
+gpp_ver=$(docker run --rm ${image}:latest g++ --version | grep g++ | awk '{print $4}')
+echo "Using g++ version ${gpp_ver}"
+
+docker tag ${image}:latest ${image}:${gpp_ver}
+docker push ${image}:latest
+docker push ${image}:${gpp_ver}
+
+echo "Done"
