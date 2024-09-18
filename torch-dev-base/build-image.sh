7b577cb4dae492729133e0e55a4f0a9a396e205a
Jianshao <787903+jianshaow@users.noreply.github.com>
2024-09-18 11:44:49 +0800

    uplift

--- torch-dev-base/build-image.sh
+++ torch-dev-base/build-image.sh
@@ -5,6 +5,9 @@ if [ "$torch_ver" == "" ]; then
 fi
 echo "Using torch version: ${torch_ver}"
 
+base_image=jianshao/torch-rt-base
+docker pull ${base_image}:${torch_ver}
+
 docker build -t jianshao/torch-dev-base:${torch_ver} . --build-arg TORCH_VER=${torch_ver} $*
 docker push jianshao/torch-dev-base:${torch_ver}
 
--- torch-rt-base/build-image.sh
+++ torch-rt-base/build-image.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ "$python_ver" == "" ]; then
+    python_ver=3.12
+fi
+echo "Using python version: ${python_ver}"
+
+base_image=jianshao/python-base
+docker pull ${base_image}:${python_ver}-slim
+
 if [ "$torch_ver" == "" ]; then
     torch_ver=2.4.1
 fi
 echo "Using torch version: ${torch_ver}"
 
-docker build -t jianshao/torch-rt-base:${torch_ver} . --build-arg TORCH_VER=${torch_ver} $*
+docker build -t jianshao/torch-rt-base:${torch_ver} . --build-arg TAG=${python_ver}-slim --build-arg TORCH_VER=${torch_ver} $*
 docker push jianshao/torch-rt-base:${torch_ver}
 
 echo "Done"
