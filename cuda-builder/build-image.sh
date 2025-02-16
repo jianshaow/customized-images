7f87b865bbb2f016f237026a1beb996da43dc6ee
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-16 11:53:17 +0800

    update builder

--- cuda-builder/build-image.sh
+++ cuda-builder/build-image.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+if [ "$python_ver" == "" ]; then
+    python_ver=3.12
+fi
+echo "Using python version: ${python_ver}"
+
+docker pull python:${python_ver}-slim
+
+if [ "$keyring_deb_url" == "" ]; then
+    keyring_deb_url=https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+fi
+
 if [ "$cuda_ver" == "" ]; then
-    cuda_ver=12.5.1
+    cuda_ver=12-4
 fi
 echo "Using cuda version: ${cuda_ver}"
 
-base_image=nvidia/cuda
-docker pull ${base_image}:${cuda_ver}-devel-ubuntu22.04
-
-image=jianshao/cuda-builder
-docker build -t ${image}:${cuda_ver} . --build-arg TAG=${cuda_ver}-devel-ubuntu22.04 $*
+image=jianshao/py-cuda-builder
+docker build -t ${image}:${cuda_ver} . --build-arg TAG=${python_ver}-slim \
+       --build-arg KEYRING_DEB_URL=${keyring_deb_url} --build-arg NVCC_PKG=cuda-nvcc-${cuda_ver} $*
 docker push ${image}:${cuda_ver}
 
 echo "Done"
