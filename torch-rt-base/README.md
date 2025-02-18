76bfdbad974d8cb67794a9b8c120c2aa71262ecb
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-18 12:11:59 +0800

    add cuda tag

--- torch-rt-base/README.md
+++ torch-rt-base/README.md
@@ -1,5 +1,7 @@
 # torch-rt-base
 
 ~~~ shell
+# export torch_ver=2.4.1
+# export cuda_tag=cu118
 ./build-image.sh
 ~~~
--- torch-rt-base/build-image.sh
+++ torch-rt-base/build-image.sh
@@ -13,7 +13,15 @@ if [ "$torch_ver" == "" ]; then
 fi
 echo "Using torch version: ${torch_ver}"
 
-docker build -t jianshao/torch-rt-base:${torch_ver} . --build-arg TAG=${python_ver}-slim --build-arg TORCH_VER=${torch_ver} $*
-docker push jianshao/torch-rt-base:${torch_ver}
+if [ "$cuda_tag" == "" ]; then
+    cuda_tag=cu124
+fi
+echo "Using torch version: ${torch_ver}"
+
+image=jianshao/torch-rt-base
+tag=${torch_ver}-${cuda_tag}
+docker build -t ${image}:${tag} . --build-arg TAG=${python_ver}-slim \
+       --build-arg TORCH_VER=${torch_ver} --build-arg CUDA_TAG=${cuda_tag} $*
+docker push ${image}:${tag}
 
 echo "Done"
