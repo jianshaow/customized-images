a0ee46d47561b86e52899f62a474958b08885302
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-02-24 17:54:21 +0800

    update

--- comfyui-rt/build-image.sh
+++ comfyui-rt/build-image.sh
@@ -13,8 +13,15 @@ if [ "$comfyui_ver" == "" ]; then
 fi
 echo "Using ComfyUI version ${comfyui_ver}"
 
+if [ "$comfyui_mgr_ver" == "" ]; then
+    comfyui_mgr_ver=3.25.1
+fi
+echo "Using ComfyUI Manager version ${comfyui_mgr_ver}"
+
 image=jianshao/comfyui-rt
-docker build -t ${image}:${comfyui_ver} . --build-arg TAG=${trfs_ver} $*
+docker build -t ${image}:${comfyui_ver} . --build-arg TAG=${trfs_ver} \
+       --build-arg COMFYUI_VER=${comfyui_ver} \
+       --build-arg COMFYUI_MGR_VER=${comfyui_mgr_ver} $*
 
 docker push ${image}:${comfyui_ver}
 
