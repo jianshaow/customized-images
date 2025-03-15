ca6ba3e81a82360f7d461501cd42dfbabd820062
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-15 17:22:53 +0800

    change base image

--- tf-rt-base/build-image.sh
+++ tf-rt-base/build-image.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-if [ "$python_ver" == "" ]; then
-    python_ver=3.12
-fi
-echo "Using python version: ${python_ver}"
-
-base_image=jianshao/python-base
-docker pull ${base_image}:${python_ver}-slim
-
 if [ "$tf_ver" == "" ]; then
     tf_ver=2.19.0
 fi
 echo "Using tensorflow version: ${tf_ver}"
 
+base_image=tensorflow/tensorflow
+docker pull ${base_image}:${tf_ver}-gpu
+
 image=jianshao/tf-rt-base
-docker build -t ${image}:${tf_ver} . --build-arg TAG=${python_ver}-slim \
+docker build -t ${image}:${tf_ver} . --build-arg TAG=${tf_ver}-gpu \
        --build-arg TF_VER=${tf_ver} $*
 docker push ${image}:${tf_ver}
 
