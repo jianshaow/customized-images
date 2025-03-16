6c8f6b1221103ce11f7d00a3095a17afb9391af0
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-17 01:20:08 +0800

    add sd webui image

---
+++ sd-webui-rt/README.md
@@ -0,0 +1,5 @@
+# comfyui-rt
+
+~~~ shell
+./build-image.sh
+~~~
---
+++ sd-webui-rt/build-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$pt_base_tag" == "" ]; then
+    pt_base_tag=2.1.2-cu121
+fi
+echo "Using torch base tag ${pt_base_tag}"
+
+base_image=jianshao/pt-rt-base
+docker pull ${base_image}:${pt_base_tag}
+
+if [ "$sd_webui_ver" == "" ]; then
+    sd_webui_ver=v0.3.23
+fi
+echo "Using SD webui version ${sd_webui_ver}"
+
+image=jianshao/sd-webui-rt
+docker build -t ${image}:${sd_webui_ver} . --build-arg TAG=${pt_base_tag} \
+       --build-arg SD_WEBUI_VER=${sd_webui_ver} $*
+
+docker push ${image}:${sd_webui_ver}
+
+echo "Done"
