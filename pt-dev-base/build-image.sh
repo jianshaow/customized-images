bec597aa6efde7c16a9c29b0c5cff0dd314d6017
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-17 00:06:57 +0800

    rename

--- pt-dev-base/build-image.sh
+++ pt-dev-base/build-image.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ "$torch_ver" == "" ]; then
-    torch_ver=2.4.1
+if [ "$pt_ver" == "" ]; then
+    pt_ver=2.4.1
 fi
-echo "Using torch version: ${torch_ver}"
+echo "Using torch version: ${pt_ver}"
 
 if [ "$cuda_tag" == "" ]; then
     cuda_tag=cu124
 fi
 echo "Using cuda tag: ${cuda_tag}"
 
-base_image=jianshao/torch-rt-base
-base_tag=${torch_ver}-${cuda_tag}
+base_image=jianshao/pt-rt-base
+base_tag=${pt_ver}-${cuda_tag}
 docker pull ${base_image}:${base_tag}
 
-image=jianshao/torch-dev-base
-tag=${torch_ver}-${cuda_tag}
+image=jianshao/pt-dev-base
+tag=${pt_ver}-${cuda_tag}
 docker build -t ${image}:${base_tag} . --build-arg TAG=${base_tag} $*
 docker push ${image}:${base_tag}
 
--- pt-rt-base/build-image.sh
+++ pt-rt-base/build-image.sh
@@ -8,20 +8,20 @@ echo "Using python version: ${python_ver}"
 base_image=jianshao/python-base
 docker pull ${base_image}:${python_ver}-slim
 
-if [ "$torch_ver" == "" ]; then
-    torch_ver=2.4.1
+if [ "$pt_ver" == "" ]; then
+    pt_ver=2.4.1
 fi
-echo "Using torch version: ${torch_ver}"
+echo "Using pytorch version: ${pt_ver}"
 
 if [ "$cuda_tag" == "" ]; then
     cuda_tag=cu124
 fi
 echo "Using cuda tag: ${cuda_tag}"
 
-image=jianshao/torch-rt-base
-tag=${torch_ver}-${cuda_tag}
+image=jianshao/pt-rt-base
+tag=${pt_ver}-${cuda_tag}
 docker build -t ${image}:${tag} . --build-arg TAG=${python_ver}-slim \
-       --build-arg TORCH_VER=${torch_ver} --build-arg CUDA_TAG=${cuda_tag} $*
+       --build-arg PT_VER=${pt_ver} --build-arg CUDA_TAG=${cuda_tag} $*
 docker push ${image}:${tag}
 
 echo "Done"
