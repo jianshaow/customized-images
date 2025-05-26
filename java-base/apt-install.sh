3e61091b83bccb60c9a5ad0648331a608075d015
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-05-26 11:29:07 +0800

    add cloudflared

--- java-base/apt-install.sh
+++ java-base/apt-install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y --no-install-recommends $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' "$1" | sort -u)
-if [ -z "$2" ]; then
-    apt-get install -y --no-install-recommends $2
+if [ $# -gt 2 ]; then
+    shift
+    apt-get install -y --no-install-recommends $@
 fi
 apt-get clean
 rm -rf /var/lib/apt/lists/*
