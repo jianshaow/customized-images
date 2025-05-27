1dd73f82dc62d3834add3f545237a2e6be69bc5a
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-05-27 11:39:01 +0800

    rollback to 22.04

--- java-base/build-image.sh
+++ java-base/build-image.sh
@@ -5,12 +5,16 @@ if [ "$java_ver" == "" ]; then
 fi
 echo "Using java version: ${java_ver}"
 
+if [ "$ubuntu_ver" == "" ]; then
+    ubuntu_ver=22.04
+fi
+echo "Using Ubuntu version: ${ubuntu_ver}"
+
 base_image=ubuntu
-tag=24.04
-docker pull ${base_image}:${tag}
+docker pull ${base_image}:${ubuntu_ver}
 
 image=jianshao/java-base
-docker build -t ${image}:${java_ver} . --build-arg TAG=${tag} $*
+docker build -t ${image}:${java_ver} . --build-arg TAG=${ubuntu_ver} $*
 docker push ${image}:${java_ver}
 
 echo "Done"
