52ad7487ea5bfc7cc3e15ae6fd33c4cbf8bf4b33
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-06-13 11:32:53 +0800

    uplift

--- comfyui-rt/build-image.sh
+++ comfyui-rt/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$trfs_ver" == "" ]; then
-    trfs_ver=4.50.3
+    trfs_ver=4.51.3
 fi
 echo "Using transformers version ${trfs_ver}"
 
@@ -9,12 +9,12 @@ base_image=jianshao/trfs-rt-base
 docker pull ${base_image}:${trfs_ver}
 
 if [ "$comfyui_ver" == "" ]; then
-    comfyui_ver=v0.3.27
+    comfyui_ver=v0.3.40
 fi
 echo "Using ComfyUI version ${comfyui_ver}"
 
 if [ "$comfyui_mgr_ver" == "" ]; then
-    comfyui_mgr_ver=3.31.8
+    comfyui_mgr_ver=3.32.5
 fi
 echo "Using ComfyUI Manager version ${comfyui_mgr_ver}"
 
