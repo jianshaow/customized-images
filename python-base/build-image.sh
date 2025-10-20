2e8204eff7d0271f814d66bed7862d4cb9319e3a
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-10-20 15:48:19 +0800

    change tag

--- python-base/build-image.sh
+++ python-base/build-image.sh
@@ -7,7 +7,7 @@ echo "Using python version: ${python_ver}"
 
 docker pull python:${python_ver}-slim
 
-docker build -t jianshao/python-base:${python_ver}-slim . --build-arg TAG=${python_ver}-slim $*
-docker push jianshao/python-base:${python_ver}-slim
+docker build -t jianshao/python-base:${python_ver} . --build-arg TAG=${python_ver}-slim $*
+docker push jianshao/python-base:${python_ver}
 
 echo "Done"
