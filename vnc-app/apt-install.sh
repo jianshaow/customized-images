a953f30de0fd4febde9df315594080081894eafc
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-01-28 12:18:30 +0800

    add upgrade

--- vnc-app/apt-install.sh
+++ vnc-app/apt-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y --no-install-recommends $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' "$1" | sort -u)
+if [ "$2" == "upgrade" ]; then
+    apt-get upgrade -y
+fi
 apt-get clean
 rm -rf /var/lib/apt/lists/*
