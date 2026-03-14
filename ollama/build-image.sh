427d2eec31cabdbccff2597664af5c7edfcd09a9
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-03-14 16:34:00 +0800

    update

--- ollama/build-image.sh
+++ ollama/build-image.sh
@@ -6,7 +6,7 @@ docker pull ${base_image}:latest
 image=jianshao/ollama
 docker build -t ${image}:latest . --build-arg TAG=latest $*
 
-ollama_ver=$(docker run --rm ${image}:latest -v | grep -oE '[0-9]+(\.[0-9]+)+')
+ollama_ver=$(docker run --rm ${image}:latest ollama -v | grep -oE '[0-9]+(\.[0-9]+)+')
 echo "Using ollama version: ${ollama_ver}"
 
 docker tag ${image}:latest ${image}:${ollama_ver}
