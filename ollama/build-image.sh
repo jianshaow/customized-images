9d9a5361632cf7822e1e597313aac21778be5689
Jianshao <787903+jianshaow@users.noreply.github.com>
2026-06-27 20:31:18 +0800

    optimize

--- ollama/build-image.sh
+++ ollama/build-image.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-ollama_tag=latest
-docker pull ollama/ollama:${ollama_tag}
+if [ "$ollama_ver" == "" ]; then
+    ollama_ver=$(curl -sL https://api.github.com/repos/ollama/ollama/releases/latest | grep tag_name | cut -d '"' -f4)
+fi
+echo "Using ollama version: ${ollama_ver}"
 
 base_tag=24.04
 docker pull ubuntu:${base_tag}
 
 image=jianshao/ollama
-docker build -t ${image}:latest . --build-arg OLLAMA_TAG=${ollama_tag} --build-arg TAG=${base_tag} $*
-
-ollama_ver=$(docker run --rm ${image}:latest ollama -v | grep -oE '[0-9]+(\.[0-9]+)+')
-echo "Using ollama version: ${ollama_ver}"
-
+docker build -t ${image}:latest . --build-arg OLLAMA_VER=${ollama_ver} --build-arg TAG=${base_tag} $*
 docker tag ${image}:latest ${image}:${ollama_ver}
 docker push ${image}:${ollama_ver}
 docker push ${image}:latest
